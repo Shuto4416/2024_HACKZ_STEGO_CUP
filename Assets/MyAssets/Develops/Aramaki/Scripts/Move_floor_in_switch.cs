6e7fa82dfafbdfdf5302801a26bb681aab683a94
Shuto.4416 <shuto.4416.git@gmail.com>
2024-06-16 03:55:24 +0900

    貫通対策、動く床のスイッチのバグ直し

--- Assets/MyAssets/Develops/Aramaki/Scripts/Move_floor_in_switch.cs
+++ Assets/MyAssets/Develops/Aramaki/Scripts/Move_floor_in_switch.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using DG.Tweening;
+using Unity.VisualScripting;
 
 public class Move_floor_in_switch : MonoBehaviour
 {
@@ -9,12 +10,13 @@ public class Move_floor_in_switch : MonoBehaviour
     [SerializeField] private float x;
     [SerializeField] private float y;
     [SerializeField] private float time;
-    Switch @switch;
+    public Switch @switch;
+    bool once = true;
     void Start()
     {
         Startposition = GetComponent<Transform>();
         MoveFloor = GetComponent<Rigidbody2D>();
-        //MoveFloor.DOMove(new Vector2(Startposition.position.x + x, Startposition.position.y + y),time).SetEase(Ease.InOutSine).SetLoops(-1,LoopType.Yoyo);
+        
     }
 
     // Update is called once per frame
@@ -23,12 +25,13 @@ public class Move_floor_in_switch : MonoBehaviour
         
     }
 
-    void OnCollisionStay2D(Collision2D other)
+    void OnCollisionEnter2D(Collision2D other)
     {
-        if(@switch == true) {
-            MoveFloor.DOMove(new Vector2(Startposition.position.x + x, Startposition.position.y + y),time).SetEase(Ease.InOutSine).SetLoops(-1,LoopType.Yoyo).Play();
-        } else {
-            MoveFloor.DOMove(new Vector2(Startposition.position.x + x, Startposition.position.y + y),time).SetEase(Ease.InOutSine).SetLoops(-1,LoopType.Yoyo).Pause();
+        if(other.gameObject.CompareTag("SlimeController") && once == true) {
+            if(@switch.button == true) {
+                MoveFloor.DOMove(new Vector2(Startposition.position.x + x, Startposition.position.y + y),time).SetEase(Ease.InOutSine).SetLoops(-1,LoopType.Yoyo);
+                once = false;
+            }
         }
     }
 }
