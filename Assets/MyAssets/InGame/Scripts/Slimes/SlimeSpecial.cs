00207ca4e9572c5945cb4aed62adc6b4ae00bb85
woods-one <0820ricer@gmail.com>
2024-06-15 20:46:27 +0900

    追加 : スペシャルの後の待機時間を追加

--- Assets/MyAssets/InGame/Scripts/Slimes/SlimeSpecial.cs
+++ Assets/MyAssets/InGame/Scripts/Slimes/SlimeSpecial.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections.Generic;
 using R3;
 using UnityEngine;
 
 namespace Assets.MyAssets.InGame.Slimes
 {
+    /// <summary>
+    /// Slimeのスペシャル攻撃
+    /// </summary>
     public class SlimeSpecial : BaseSlimeComponent
     {
         [SerializeField]
@@ -11,15 +15,23 @@ namespace Assets.MyAssets.InGame.Slimes
 
         [SerializeField] 
         private Transform _slimeTransform;
+
+        private bool _canSpecial;
         
         protected override void OnInitialize()
         {
+            _canSpecial = true;
             InGameInputEventProvider.OnSpecialButtonPushed
-                .Where(x => x)
+                .Where(x => x && _canSpecial)
                 .Skip(1)
                 .Subscribe(_ =>
                 {
                     Instantiate(_slimeSpecials[(int)(SlimeCore.SpecialTypes)], _slimeTransform.position, Quaternion.identity);
+
+                    _canSpecial = false;
+                    
+                    Observable.Timer(TimeSpan.FromSeconds(SlimeCore.SpecialTypes.ToWaitSeconds()))
+                        .Subscribe(_ => _canSpecial = true);
                 });
         }
     }
