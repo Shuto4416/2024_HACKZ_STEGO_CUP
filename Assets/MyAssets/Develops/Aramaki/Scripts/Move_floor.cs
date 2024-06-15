5fa545777b2663246051905ea0e365ee405c3ffe
Shuto.4416 <shuto.4416.git@gmail.com>
2024-06-15 22:04:27 +0900

    地下ステージ作成、天空ステージ作成、動く足場作成

---
+++ Assets/MyAssets/Develops/Aramaki/Scripts/Move_floor.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class Move_floor : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D MoveFloor;
+    [SerializeField] private float x;
+    [SerializeField] private float y;
+    [SerializeField] private float time;
+    void Start()
+    {
+        Transform Startposition = GetComponent<Transform>();
+        MoveFloor = GetComponent<Rigidbody2D>();
+        MoveFloor.DOMove(new Vector2(Startposition.position.x + x, Startposition.position.y + y),time).SetEase(Ease.InOutSine).SetLoops(-1,LoopType.Yoyo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
