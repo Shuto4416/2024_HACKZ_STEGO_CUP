592d964246d7f24819d9db9f3905238e2c347bd0
woods-one <0820ricer@gmail.com>
2024-06-15 17:28:52 +0900

    追加 : スライムの属性攻撃を追加

---
+++ Assets/MyAssets/InGame/Scripts/Slimes/SlimeSpecial.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Assets.MyAssets.InGame.Slimes
+{
+    public class SlimeSpecial : BaseSlimeComponent
+    {
+        protected override void OnInitialize()
+        {
+            //InGameInputEventProvider.OnSpecialButtonPushed
+        }
+    }
+}
