1f6f837827a9b19f9188cf095d05658000f73d3e
woods-one <0820ricer@gmail.com>
2024-06-16 07:56:21 +0900

    追加 : 各シーンの値受け渡しクラスの実装

--- Assets/MyAssets/Common/Scripts/Scenes/MakeSlime.cs
+++ Assets/MyAssets/Common/Scripts/Scenes/MakeSlime.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 using System.Threading.Tasks;
+using Assets.MyAssets.InGame.Slimes;
 
 namespace Assets.MyAssets.Common.Scripts.Scenes
 {
     public class MakeSlime : MonoBehaviour
     {
-        public async void PassMainToGameOver()
+        public async void PassMakeSlimeToInGame(float weight,float size, float viscosity, SpecialTypes specialTypes)
         {
             await Task.Delay(3000);
-            //var sceneB = await SceneLoader.Load<GameOver>("GameOver");
-            //sceneB.SetArguments(battleGameManager.enemyKillNum - 1, charaName);
+            var nextScene = await SceneLoader.Load<InGame>("InGame");
+            nextScene.SetArguments(weight,size,viscosity,specialTypes);
         }
     }
 }
