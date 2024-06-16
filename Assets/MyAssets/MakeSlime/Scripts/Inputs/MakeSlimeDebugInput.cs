6679ff0949f919802047407381d5a954f6dade11
woods-one <0820ricer@gmail.com>
2024-06-16 13:24:42 +0900

    追加 : 矢印でロールできるようにしました

--- Assets/MyAssets/MakeSlime/Scripts/Inputs/MakeSlimeDebugInput.cs
+++ Assets/MyAssets/MakeSlime/Scripts/Inputs/MakeSlimeDebugInput.cs
@@ -1,67 +1,81 @@
 using System;
+using Assets.MyAssets.MakeSlime.Inputs;
 using R3;
 using R3.Triggers;
 using UnityEngine;
 
-public class MakeSlimeDebugInput : MonoBehaviour
+namespace Assets.MyAssets.InGame.Slimes.Inputs
 {
-    private ReactiveProperty<bool> _upButton;
-    private ReactiveProperty<bool> _downButton;
-    private ReactiveProperty<bool> _rightButton;
-    private ReactiveProperty<bool> _leftButton;
-    private ReactiveProperty<bool> _decideButton;
-    private ReactiveProperty<bool> _returnButton;
-    
-    ReadOnlyReactiveProperty<bool> UpButton
+    public class MakeSlimeDebugInput : MonoBehaviour,IMakeSlimeInputEventProvider
     {
-        get { return _upButton; }
-    }
-    ReadOnlyReactiveProperty<bool> DownButton     
-    {
-        get { return _downButton; }
-    }
-    ReadOnlyReactiveProperty<bool> RightButton     
-    {
-        get { return _rightButton; }
-    }
-    ReadOnlyReactiveProperty<bool> LeftButton   
-    {
-        get { return _leftButton; }
-    }
-    ReadOnlyReactiveProperty<bool> DecideButton     
-    {
-        get { return _decideButton; }
-    }
-    ReadOnlyReactiveProperty<bool> ReturnButton    
-    {
-        get { return _returnButton; }
-    }
-    
-    void Start()
-    {
-        this.UpdateAsObservable()
-            .Select(_ => Input.GetKey(KeyCode.W))
-            .DistinctUntilChanged()
-            .Subscribe(x => _upButton.Value = x);
-        this.UpdateAsObservable()
-            .Select(_ => Input.GetKey(KeyCode.S))
-            .DistinctUntilChanged()
-            .Subscribe(x => _downButton.Value = x);
-        this.UpdateAsObservable()
-            .Select(_ => Input.GetKey(KeyCode.D))
-            .DistinctUntilChanged()
-            .Subscribe(x => _rightButton.Value = x);
-        this.UpdateAsObservable()
-            .Select(_ => Input.GetKey(KeyCode.A))
-            .DistinctUntilChanged()
-            .Subscribe(x => _leftButton.Value = x);           
-        this.UpdateAsObservable()
-            .Select(_ => Input.GetKey(KeyCode.Space))
-            .DistinctUntilChanged()
-            .Subscribe(x => _decideButton.Value = x);   
-        this.UpdateAsObservable()
-            .Select(_ => Input.GetKey(KeyCode.Escape))
-            .DistinctUntilChanged()
-            .Subscribe(x => _returnButton.Value = x); 
+        private ReactiveProperty<bool> _upButton = new ReactiveProperty<bool>(false);
+        private ReactiveProperty<bool> _downButton = new ReactiveProperty<bool>(false);
+        private ReactiveProperty<bool> _rightButton = new ReactiveProperty<bool>(false);
+        private ReactiveProperty<bool> _leftButton = new ReactiveProperty<bool>(false);
+        private ReactiveProperty<bool> _decideButton = new ReactiveProperty<bool>(false);
+        private ReactiveProperty<bool> _returnButton = new ReactiveProperty<bool>(false);
+
+        public ReadOnlyReactiveProperty<bool> UpButton
+        {
+            get { return _upButton; }
+        }
+
+        public ReadOnlyReactiveProperty<bool> DownButton
+        {
+            get { return _downButton; }
+        }
+
+        public ReadOnlyReactiveProperty<bool> RightButton
+        {
+            get { return _rightButton; }
+        }
+
+        public ReadOnlyReactiveProperty<bool> LeftButton
+        {
+            get { return _leftButton; }
+        }
+
+        public ReadOnlyReactiveProperty<bool> DecideButton
+        {
+            get { return _decideButton; }
+        }
+
+        public ReadOnlyReactiveProperty<bool> ReturnButton
+        {
+            get { return _returnButton; }
+        }
+
+        void Start()
+        {
+            this.UpdateAsObservable()
+                .Select(_ => Input.GetKey(KeyCode.W))
+                .DistinctUntilChanged()
+                .Subscribe(x => _upButton.Value = x);
+
+            this.UpdateAsObservable()
+                .Select(_ => Input.GetKey(KeyCode.S))
+                .DistinctUntilChanged()
+                .Subscribe(x => _downButton.Value = x);
+
+            this.UpdateAsObservable()
+                .Select(_ => Input.GetKey(KeyCode.D))
+                .DistinctUntilChanged()
+                .Subscribe(x => _rightButton.Value = x);
+
+            this.UpdateAsObservable()
+                .Select(_ => Input.GetKey(KeyCode.A))
+                .DistinctUntilChanged()
+                .Subscribe(x => _leftButton.Value = x);
+
+            this.UpdateAsObservable()
+                .Select(_ => Input.GetKey(KeyCode.Space))
+                .DistinctUntilChanged()
+                .Subscribe(x => _decideButton.Value = x);
+
+            this.UpdateAsObservable()
+                .Select(_ => Input.GetKey(KeyCode.Escape))
+                .DistinctUntilChanged()
+                .Subscribe(x => _returnButton.Value = x);
+        }
     }
 }
